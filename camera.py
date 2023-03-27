64fcbd164d650f39e3518ba4f2afdf178f396493
Gautam2000 <63543004+Gautam-MV@users.noreply.github.com>
2023-03-27 08:28:46 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -12,9 +12,6 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.engine import DefaultTrainer
 from detectron2.data.datasets import register_coco_instances
 
-import zipfile
-with zipfile.ZipFile("static/uploads/model_final.zip","r") as zip_ref:
-    zip_ref.extractall("static")
 
 class VideoCamera(object):
     def __init__(self,filename):
