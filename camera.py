f664d1e9c937a0a184c99e970fb20e9a7ea380d6
Gautam2000 <63543004+Gautam-MV@users.noreply.github.com>
2023-03-27 08:12:04 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -61,7 +61,7 @@ class VideoCamera(object):
         v = Visualizer(image[:, :, ::-1],
                        metadata=sample_metadata, 
                        scale=0.8, 
-                       instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels)
+                       instance_mode=ColorMode.IMAGE_BW)   # remove the colors of unsegmented pixels)
         v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         image = v.get_image()[:, :, ::-1]
         ret, jpeg = cv2.imencode('.jpg', image)
